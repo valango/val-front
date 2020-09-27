463b51b7867b1d00477f6fc17572447ea5ea755c
Villem Alango <villem.alango@gmail.com>
2020-09-27 17:25:37 +0300

    removed: unneeded dependencies

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// https://jestjs.io/docs/en/configuration
-module.exports = {
-  // collectCoverage: true,
-  collectCoverageFrom: ['*.js', '!*.config.js', '!.*'],
-  coverageDirectory: 'reports',
-  coveragePathIgnorePatterns: ['<rootDir>/index', 'reports', 'test'],
-  coverageProvider: 'babel',
-  verbose: true
-}
